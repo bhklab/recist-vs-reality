017beba6816a703171376f0b153243bc4bdf88e6
Katy Scott <k.l.scott16@gmail.com>
2025-06-04 19:46:49 +0000

    feat: add function to process entire dataset of SRs

--- workflow/scripts/sr_testing.py
+++ workflow/scripts/sr_testing.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import pydicom
 import click
+from damply import dirs
+from pathlib import Path
+from readii.io.loaders import loadImageDatasetConfig
 
 def find_graphic_data(dataset: pydicom.Dataset,
                       file_path: str,
-                      current_references: list | None = None, 
-                      graphic_data_list: dict | None = None, 
-                      ) -> pd.DataFrame:
+                      current_references: dict | None = None, 
+                      graphic_data_list: list | None = None, 
+                      ) -> list[dict]:
     """
     Recursively searches through a DICOM dataset to find and extract graphic data and related references.
     """
     if current_references is None:
-        current_references = []
+        current_references = {}
 
     if graphic_data_list is None:
         graphic_data_list = []
@@ -63,17 +66,60 @@ def find_graphic_data(dataset: pydicom.Dataset,
             current_references['ContentDescription'] = elem.value
 
 
-def extract_graphic_data_with_references(file_path: str):
+def extract_graphic_data_with_references(file_path: str) -> list[dict]:
     """
     Extract all graphic data and their references from a DICOM SR file.
     """
     ds = pydicom.dcmread(file_path)
     graphic_data_list = []
-    find_graphic_data(ds, file_path, {}, graphic_data_list)
-    return pd.DataFrame(graphic_data_list)
+    find_graphic_data(ds, file_path, graphic_data_list = graphic_data_list)
+    return graphic_data_list
 
 
-# Example usage
-dicom_file_path = "data/rawdata/TCIA_CPTAC-HNSCC/images/annotations/CPTAC-HNSCC/animated.peafowl-CPTAC-HNSCC-C3N-00828-1.3.6.1.4.1.14519.5.2.1.3320.3273.324519151134894128406738207272.dcm"
-graphic_data_df = extract_graphic_data_with_references(dicom_file_path)
-print(graphic_data_df)
+@click.command
+@click.option('--dataset', help='Name of dataset to process. Must have config file in config directory.')
+@click.option('--overwrite', help='Whether to overwrite existing readii image files', default=False)
+def scrape_annotation_dataset(dataset:str,
+                              overwrite:bool = False) -> pd.DataFrame:
+    """
+    Scrape all DICOM SR files for a given dataset to make an index file.
+    """
+    # Load in configuration settings for dataset
+    config = loadImageDatasetConfig(dataset, dirs.CONFIG)
+
+    # Set up output path
+    out_path = dirs.PROCDATA / f"{config['DATA_SOURCE']}_{config['DATASET_NAME']}" / "annotations" / f"{config['DATASET_NAME']}_graphic_data.csv"
+
+    # Check if output already exists
+    if out_path.exists() and not overwrite:
+        print(f"{dataset} annotations have already had their graphic data scraped. See {out_path}. Set overwrite to True to force scrape. Loading existing file.")
+        dataset_graphic_df = pd.read_csv(out_path)
+
+    else:
+        # Create output path directories
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Set up path to annotation files
+        annotation_dir_path = dirs.RAWDATA / f"{config['DATA_SOURCE']}_{config['DATASET_NAME']}" / "images" / "annotations" / f"{config['DATASET_NAME']}"
+        # Initialize empty list to store graphic data in for each file
+        dataset_graphic_data = []
+        # Iterate over the annotation files, scrape their graphic data, and save the list in the global list. Should end up as a list of dictionaries.
+        for file_path in annotation_dir_path.glob("*.dcm"):
+            dataset_graphic_data += (extract_graphic_data_with_references(file_path))
+        
+        # Convert list of dictionaries to a dataframe
+        dataset_graphic_df = pd.DataFrame(dataset_graphic_data)
+        # Save out dataframe
+        dataset_graphic_df.to_csv(out_path, index=False)
+
+    return dataset_graphic_df
+    
+        
+
+if __name__ == '__main__':
+    # Example usage
+    # dicom_file_path = "data/rawdata/TCIA_CPTAC-HNSCC/images/annotations/CPTAC-HNSCC/animated.peafowl-CPTAC-HNSCC-C3N-00828-1.3.6.1.4.1.14519.5.2.1.3320.3273.324519151134894128406738207272.dcm"
+    # graphic_data_df = extract_graphic_data_with_references(dicom_file_path)
+    # print(graphic_data_df)
+
+    scrape_annotation_dataset()
