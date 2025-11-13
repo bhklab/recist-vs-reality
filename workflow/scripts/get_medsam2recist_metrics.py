623a925ead7ef8b43d9132a167e63aeec74ce006
kaitlyn-kobayashi <kaitlynshizuko-mary.kobayashi@uhn.ca>
2025-11-13 21:35:30 +0000

    refactor: make file names relative when adding to dataframe

--- workflow/scripts/get_medsam2recist_metrics.py
+++ workflow/scripts/get_medsam2recist_metrics.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from evaluate import Evaluator 
 from damply import dirs
 
-def compile_metrics(results_path: Path, 
-                        save_path: Path): 
+def compile_metrics_eff(results_path: Path, 
+                    save_path: Path, 
+                    ): 
     '''
-    Calculate performance metrics of all generated segmentations from MedSAM2-RECIST model. Results must
+    Calculate performance metrics of all generated segmentations from MedSAM2-RECIST models. Results must
     be in .npz format with the following keys: segs, gts, boxes, spacing
 
     Parameters
@@ -20,13 +21,14 @@ def compile_metrics(results_path: Path,
     '''
     # Loop over all of the data within specified folder 
     for file in results_path.iterdir(): 
-        #Ignore files that are not npz 
+        # Ignore files that are not npz 
         if not str(file).endswith('.npz'): 
             continue 
-
+        
+        # Load in npz data 
         seg_model_result = np.load(file)
 
-        #Get data required for metrics calculation 
+        # Get data required for metrics calculation 
         gen_seg = seg_model_result['segs']
         grd_truth_seg = seg_model_result['gts']
         seg_spacing = seg_model_result['spacing']
@@ -39,7 +41,10 @@ def compile_metrics(results_path: Path,
         
         # Convert dictionary to dataframe, add filename info, and add performance to overall dataframe 
         curr_metric_df = pd.DataFrame(curr_metric_dict, index = [0]) 
-        curr_metric_df['filename'] = str(file)
+        
+        # Save relative path starting from dataset folder 
+        rel_file = '/'.join(str(file).split('/')[-4:])
+        curr_metric_df['filename'] = rel_file
 
         if 'all_metrics_df' not in locals(): # Check if dataframe variable doesn't exist yet
             all_metrics_df = curr_metric_df
@@ -50,13 +55,13 @@ def compile_metrics(results_path: Path,
     if not save_path.exists(): 
         Path(save_path.mkdir(parents = True, exist_ok = True))
 
-    save_name = save_path / "medsam2recist_seg_eval.csv"
+    save_name = save_path / "medsam2recisteff_seg_eval.csv"
 
     all_metrics_df.to_csv(save_name)
 
 if __name__ == '__main__': 
     seg_path = Path('/home/bhkuser/bhklab/radiomics/Projects/MedSAM2-RECIST/data/results/RADCURE_OCSCC/predictions/eff-tiny')
     save_path = dirs.RESULTS / Path('RADCURE_OCSCC/metric_eval')
-    compile_metrics(results_path = seg_path, 
+    compile_metrics_eff(results_path = seg_path, 
                     save_path = save_path)
         
